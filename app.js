7735487786f36f559b7892635daba56f158ef1b9
Chisty07 <chistysaifuddin@gmail.com>
2023-03-09 11:28:45 +0600

    This is my 1st git

--- app.js
+++ app.js
@@ -18,17 +18,37 @@ let someFn = (element) => element % 2 == 0;
 console.log(arr.some(someFn)); // some return boolean value
 
 //split
+//let splitarr = arr.split('',2);
+//console.log(splitarr);
 
 //slice
+let sliceFn =(element)=> element;
+console.log(arr.slice(sliceFn,2));
 
 //sort
 
+console.log(arr.sort(function(a,b){return(b-a)}));
+
 //foreach
+arr.forEach(arr => console.log(arr));
 
 //indexof
+console.log(arr.indexOf(3));
 
 //includes (try it but we will discuss it later)
+console.log(arr.includes(2));
 
 //reduce (try it but we will discuss it later)
+let joinarr = (accumulator, currentvalue) => accumulator+ currentvalue;
+console.log(arr.reduce(joinarr));
 
 //join (we will discuss it in string topic)
+let join = arr.join("+");
+console.log(join);
+
+//stack in array
+let num = [];
+let digit = num.push(4, 2);
+console.log(digit);
+console.log(num);
+
