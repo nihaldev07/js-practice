f852fb4e8a0a0304d9f7da80611f42f193d1e334
Md. Minhaz Uddin <ni.xeron07@gmail.com>
2023-03-09 12:07:29 +0600

    update | string manupulation

---
+++ arrFn.js
@@ -0,0 +1,8 @@
+const arr = ["a", "b", "c", "d", "e", "f"];
+//console.log(arr.slice(1, -2)); // slice returns array
+
+const fteam = "Manchester United is a football team";
+let newArr = fteam.split(" ");
+console.log(newArr); //split returns array
+
+console.log(newArr.join(" "));
