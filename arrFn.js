d7a124c175208f581fc051e9780b9764dadc88c4
Md. Minhaz Uddin <ni.xeron07@gmail.com>
2023-03-09 12:31:02 +0600

    prblm

--- arrFn.js
+++ arrFn.js
@@ -2,7 +2,19 @@ const arr = ["a", "b", "c", "d", "e", "f"];
 //console.log(arr.slice(1, -2)); // slice returns array
 
 const fteam = "Manchester United is a football team";
-let newArr = fteam.split(" ");
-console.log(newArr); //split returns array
+//let newArr = fteam.split(" ");
+//console.log(newArr); //split returns array
 
-console.log(newArr.join(" "));
+const vowel = ["a", "e", "i", "o", "u"];
+
+const newArr = fteam.split(" ");
+const filtered = newArr.filter(
+  (item) => !vowel.includes(item[0].toLowerCase())
+);
+const finalString = filtered.join(" ");
+console.log(
+  fteam
+    .split(" ")
+    .filter((item) => !vowel.includes(item[0].toLowerCase()))
+    .join(" ")
+);
